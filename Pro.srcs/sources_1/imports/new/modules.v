62cee4f0035b613d529275207ab6fa523a5280d4
KevinWorkSpace <30995513+KevinWorkSpace@users.noreply.github.com>
2018-12-25 07:32:29 +0800

    修正bug

--- Pro.srcs/sources_1/imports/new/modules.v
+++ Pro.srcs/sources_1/imports/new/modules.v
@@ -1,5 +1,54 @@
 `timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2018/12/24 19:36:15
+// Design Name: 
+// Module Name: decoder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
+module key_fangdou(clk,key_in,key_out);
+    parameter SAMPLE_TIME = 4;
+    input clk;
+    input key_in;
+    output key_out;
+    
+    reg [2:0] count_low;
+    reg [2:0] count_high;  
+    reg key_out_reg;
+
+    always @(posedge clk)
+        if(key_in ==1'b0)
+            count_low <= count_low + 1;
+        else
+            count_low <= 0;
+    
+    always @(posedge clk)
+        if(key_in ==1'b1)
+            count_high <= count_high + 1;
+        else
+            count_high <= 0;
+    
+    always @(posedge clk)
+        if(count_high == SAMPLE_TIME)
+            key_out_reg <= 1;
+        else if(count_low == SAMPLE_TIME)
+            key_out_reg <= 0;
+    
+    assign key_out = key_out_reg;
+endmodule 
 module Highdecoder(
 input [3:0] high_in,
 output reg [7:0] high_out 
@@ -19,12 +68,12 @@ always @(high_in)
 endmodule
 
 module Lowdecoder(
-input [3:0] lownum_in,
+input [3:0] low_in,
 output reg [7:0] low_out 
 );
-always @(lownum_in)
+always @(low_in)
     begin
-     case(lownum_in)
+     case(low_in)
         4'd0: low_out = 8'b01000000;
         4'd1: low_out = 8'b01111001;
         4'd2: low_out = 8'b00100100;
@@ -39,18 +88,21 @@ always @(lownum_in)
     end
 endmodule
 
+
 module secondconter(
-    input clk_1hz, rst,rstall,
-    output reg clkout,
-    output reg [3:0] secL,
-    output reg [3:0] secH
+input clk_1hz, rst,rstall,
+output reg clkout,
+output reg [3:0] secL,
+output reg [3:0] secH
+
     );
-    always @(posedge clk_1hz,negedge rst,negedge rstall)
+   
+    always @(posedge clk_1hz,posedge rst,posedge rstall)
     begin 
-    if(!rst & rstall)   //绉掕鏁板櫒缃浂淇″彿
+    if(rst || rstall)   //秒计数器置零信号
         {secH,secL}<=8'd0;
         
-    else if(secH==5&&secL==9) //璁℃暟鍒?59s
+    else if(secH==5&&secL==9) //计数到59s
         begin
         {secH,secL}<=8'd0;
         clkout <= 1;
@@ -60,18 +112,78 @@ module secondconter(
         clkout <= 0;
         secL<=secL+4'd1;
         end
-    else if(secL==4'd9) //鍏朵粬鐨?9s
+    else if(secL==4'd9) //其他的9s
         begin 
          secL<=4'd0; 
          secH<=secH+4'd1;
         end 
-    else    //涓?鑸儏鍐?
+    else    //一般情况
         secL<=secL+4'd1; 
     end 
     
 endmodule
 
 
+module hourconter(
+input clk_minin, reset,
+output reg [3:0] hourL,
+output reg [3:0] hourH
+
+    );
+    always @(posedge clk_minin,posedge reset)
+    begin 
+    if(reset)   //分计数器置零信号
+        {hourH,hourL}<=8'd0;
+        
+    else if(hourH==2&&hourL==3) //计数到23hour
+        {hourH,hourL}<=8'd0;
+    else if(hourL==4'd9) //9hour
+        begin 
+        hourL<=4'd0; 
+        hourH<=hourH+4'd1;
+        end
+    else    //一般情况
+        hourL <= hourL+4'd1; 
+    end 
+    
+endmodule
+
+
+module minuteconter(
+input clk_secin, resetall,
+output reg clkout,
+output reg [3:0] minL,
+output reg [3:0] minH
+
+    );
+    always @(posedge clk_secin,posedge resetall)
+    begin 
+    if(resetall)   //分计数器置零信号
+        {minH,minL}<=8'd0;
+        
+    else if(minH==5&&minL==9) //计数到59min
+        begin
+        {minH,minL}<=8'd0;
+        clkout <= 1;
+        end
+    else if(minH==0&&minL==0)
+        begin
+        clkout <= 0;
+        minL <= minL+4'd1;
+        end
+    else if(minL==4'd9) //其他的9min
+        begin 
+         minL<=4'd0; 
+         minH<=minH+4'd1;
+        end 
+    else    //一般情况
+        minL <= minL+4'd1; 
+    end 
+   
+endmodule
+
+
+`timescale 1ns / 1ps
 
 module frequencyDivider(
 clk,rst_n,hz1out,hz4out,hz64out,hz500out,hz1000out
@@ -79,26 +191,26 @@ clk,rst_n,hz1out,hz4out,hz64out,hz500out,hz1000out
     input clk,rst_n;
     output reg hz1out,hz4out,hz64out,hz500out,hz1000out;
     reg [27:0]cnt1,cnt4,cnt64,cnt500,cnt1000;
-    parameter hz1=4,hz4=25000000,hz64=1562500,hz500=200000,hz1000=100000;
+    parameter hz1=100000000,hz4=25000000,hz64=1562500,hz500=200000,hz1000=100000;
    //1 
-    always@(posedge clk,negedge rst_n)
+    always@(posedge clk,posedge rst_n)
     begin
-        if(~rst_n)begin
-            cnt1<=0;
-            hz1out<=0;
-        end
-        else if(cnt1==(hz1>>1)-1)
-        begin
-            cnt1<=0;
-            hz1out<=~hz1out;
-        end
-        else
-            cnt1<=cnt1+1;
+    if(rst_n)begin
+    cnt1<=0;
+    hz1out<=0;
+    end
+    else if(cnt1==(hz1>>1)-1)
+    begin
+    cnt1<=0;
+    hz1out<=~hz1out;
+    end
+    else
+    cnt1<=cnt1+1;
     end
   //4  
-     always@(posedge clk,negedge rst_n)
+     always@(posedge clk,posedge rst_n)
        begin
-       if(~rst_n)begin
+       if(rst_n)begin
        cnt4<=0;
        hz4out<=0;
        end
@@ -112,9 +224,9 @@ clk,rst_n,hz1out,hz4out,hz64out,hz500out,hz1000out
        end
        
        //64
-        always@(posedge clk,negedge rst_n)
+        always@(posedge clk,posedge rst_n)
           begin
-          if(~rst_n)begin
+          if(rst_n)begin
           cnt64<=0;
           hz64out<=0;
           end
@@ -126,129 +238,71 @@ clk,rst_n,hz1out,hz4out,hz64out,hz500out,hz1000out
           else
           cnt64<=cnt64+1;
           end
-     //500     
-   always@(posedge clk,negedge rst_n) begin
-         if(~rst_n)begin
+//500     
+           always@(posedge clk,posedge rst_n)
+             begin
+             if(rst_n)begin
              cnt500<=0;
              hz500out<=0;
-         end
-         else if(cnt500==(hz500>>1)-1)
-         begin
+             end
+             else if(cnt500==(hz500>>1)-1)
+             begin
              cnt500<=0;
              hz500out<=~hz500out;
-         end
-         else
-            cnt500<=cnt500+1;
-         end
-	 //1000 
-    always@(posedge clk,negedge rst_n)
+             end
+             else
+             cnt500<=cnt500+1;
+             end
+//1000 
+    always@(posedge clk,posedge rst_n)
     begin
-    if(~rst_n)begin
-        cnt1000<=0;
-        hz1000out<=0;
+    if(rst_n)begin
+    cnt1000<=0;
+    hz1000out<=0;
     end
     else if(cnt1000==(hz1000>>1)-1)
     begin
-        cnt1000<=0;
-        hz1000out<=~hz1000out;
+    cnt1000<=0;
+    hz1000out<=~hz1000out;
     end
     else
-        cnt1000<=cnt1000+1;
+    cnt1000<=cnt1000+1;
     end
              
     
 endmodule
 
 
-module controlcircuit(
-    setSecond,setMinute,setHour,Clk,reset,toSecond,toMinute,toHour,setZero
+
+`timescale 1ns / 1ps
+
+module ControlUnit(
+setSecond,setMinute,setHour,Clk,reset,toSecond,toMinute,toHour,setZero
     );
     
     input setSecond,setMinute,setHour,Clk,reset;
-    output reg toSecond,toMinute,toHour,setZero;
+    output toSecond,toMinute,toHour,setZero;
+    reg toSecond,toMinute,toHour,setZero;
     
-    always@(posedge Clk,posedge reset) begin
-        if(reset==1)
-            begin
-                toSecond<=0;
-                toMinute<=0;
-                toHour<=0;
-                setZero<=1;
-            end
-        else
-            begin
-                toSecond<=setSecond;
-//                toSecond <= 0;
-                toMinute<=setMinute;
-                toHour<=setHour;
-                setZero<=0;
-            end
+    always@(posedge Clk,posedge reset)
+    if(reset==1)
+    begin
+    toSecond<=0;
+    toMinute<=0;
+    toHour<=0;
+    setZero<=reset;
     end
- 
-endmodule
-
-
-module minuteconter(
-input clk_secin, reset,
-output reg clkout,
-output reg [3:0] minL,
-output reg [3:0] minH
-
-    );
-    always @(posedge clk_secin,negedge reset)
-    begin 
-    if(!reset)   //鍒嗚鏁板櫒缃浂淇″彿
-        {minH,minL}<=8'd0;
-        
-    else if(minH==5&&minL==9) //璁℃暟鍒?59min
-        begin
-        {minH,minL}<=8'd0;
-        clkout <= 1;
-        end
-    else if(minH==0&&minL==0)
-        begin
-        clkout <= 0;
-        minL <= minL+4'd1;
-        end
-    else if(minL==4'd9) //鍏朵粬鐨?9min
-        begin 
-         minL<=4'd0; 
-         minH<=minH+4'd1;
-        end 
-    else    //涓?鑸儏鍐?
-        minL <= minL+4'd1; 
-    end 
-    
-    
-    
+    else
+    begin
+    toSecond<=setSecond;
+    toMinute<=setMinute;
+    toHour<=setHour;
+    setZero<=reset;
+    end
 endmodule
 
 
 
-module hourconter(
-input clk_minin, reset,
-output reg [3:0] hourL,
-output reg [3:0] hourH
-
-    );
-    always @(posedge clk_minin,negedge reset)
-    begin 
-    if(!reset)   //鍒嗚鏁板櫒缃浂淇″彿
-        {hourH,hourL}<=8'd0;
-        
-    else if(hourH==2&&hourL==3) //璁℃暟鍒?23hour
-        {hourH,hourL}<=8'd0;
-    else if(hourL==4'd9) //9hour
-        begin 
-        hourL<=4'd0; 
-        hourH<=hourH+4'd1;
-        end
-    else    //涓?鑸儏鍐?
-        hourL <= hourL+4'd1; 
-    end 
-    
-endmodule
- 
 
 module clock(sech,secl,minh,minl,hz500clk,hz1Kclk,clkout);
 
@@ -259,32 +313,37 @@ input [3:0] minl;
 input hz500clk,hz1Kclk;
 output clkout;
 reg clkout;
-always@(minh,minl,sech,secl,hz500clk,hz1Kclk)
-    if (minh==4'd5 && minl==4'd9 && sech==4'd5)
-        case(secl)
-            4'd0,8'd52,4'd4,4'd6,4'd8 : clkout <= hz500clk;
-            default : clkout <= 1'b0;
-        endcase
-    else if({minh,minl}==8'd00 && {sech,secl}==8'd00)
-        clkout <= hz1Kclk;
-    else 
-        clkout <=1'b0;
+always@*
+begin
+    if(minh==4'b0101 && minl==4'b1001)
+        begin
+            if(sech==4'b0101)
+            begin
+            if(secl==4'b0000 | secl==4'b0010|secl==4'b0100|secl==4'b1000|secl==4'b0110)
+            begin
+                clkout <= hz500clk;
+                #500 clkout<=0;
+            end
+            end
+         end
+     else if (minh==4'b0000 && minl ==4'b0000 && sech==4'b0000 && secl==4'b0000)
+            clkout<=hz1Kclk;
+     else
+        clkout<= 1'b0;
+end
 endmodule
 
-
+`timescale 1ns / 1ps
 
 module MUXforMinute(
 Clk,SecondCarry,Control,countPlus
     );
     input Clk,SecondCarry,Control;
     output countPlus;
-    reg countPlus;
-    
-   always@(Control,Clk,SecondCarry)
-   case(Control)
-   1'b1: countPlus=Clk;
-   1'b0: countPlus=SecondCarry;
-   endcase
+    wire countPlus;
+
+   assign countPlus=(Control)?Clk:SecondCarry;
+ 
    
 endmodule
 
@@ -294,11 +353,68 @@ Clk,MinuteCarry,Control,countPlus
     );
     input Clk,MinuteCarry,Control;
     output countPlus;
-    reg countPlus;
-    always@(Control,Clk,MinuteCarry)
-    case(Control)
-    1'b1: countPlus=Clk;
-    1'b0: countPlus=MinuteCarry;
-    endcase
+    wire countPlus;
+
         
+        assign countPlus=(Control)?Clk:MinuteCarry;
+endmodule
+
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2018/12/16 15:50:31
+// Design Name: 
+// Module Name: seg
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module seg(
+    input[7:0] hour1,
+    input[7:0] hour2,
+    input[7:0] min1,
+    input[7:0] min2,
+    input[7:0] sec1,
+    input[7:0] sec2,
+    input clk, rst_n,
+    output reg [7:0] scan_cnt,   //8个数码管的使能信号
+    output reg [7:0] seg_out     //8个数码管显示的数字
+    );
+    always @(posedge clk, posedge rst_n) begin
+        if(rst_n)
+            scan_cnt = 8'b1111_1110;
+        else begin
+            case(scan_cnt)
+                8'b1111_1110: scan_cnt = 8'b1111_1101;
+                8'b1111_1101: scan_cnt = 8'b1111_1011;
+                8'b1111_1011: scan_cnt = 8'b1111_0111;
+                8'b1111_0111: scan_cnt = 8'b1110_1111;
+                8'b1110_1111: scan_cnt = 8'b1101_1111;
+                8'b1101_1111: scan_cnt = 8'b1111_1110;
+            endcase
+        end
+    end
+    
+    always @(scan_cnt)
+    begin
+        case(scan_cnt)
+            8'b1111_1110: seg_out = sec2;
+            8'b1111_1101: seg_out = sec1;
+            8'b1111_1011: seg_out = min2;
+            8'b1111_0111: seg_out = min1;
+            8'b1110_1111: seg_out = hour2;
+            8'b1101_1111: seg_out = hour1;
+       endcase
+    end
 endmodule
--- Pro.srcs/sources_1/new/top.v
+++ Pro.srcs/sources_1/new/top.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 2018/12/20 19:44:45
+// Create Date: 2018/12/24 19:44:00
 // Design Name: 
 // Module Name: top
 // Project Name: 
@@ -20,7 +20,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module allfile(
+module top(
     input rstsecond,resetall,setmin,sethour,clkdefoult,
     output[7:0] seg_out, scan_cnt,
     output clk_buzzer
@@ -40,12 +40,12 @@ module allfile(
     key_fangdou k1(hz64out,setmin,out_min);
     key_fangdou k2(hz64out,sethour,out_hour);
     key_fangdou k3(hz64out,rstsecond,out_sec); 
-    key_fangdou k5(hz64out,resetall,out_rstall);   
+    key_fangdou k4(hz64out,resetall,out_rstall);   
     
     frequencyDivider u1(clkdefoult,resetall,hz1out,hz4out,hz64out,hz500out,hz1000out);
     
     wire toSecond,toMinute,toHour,setZero;
-    controlcircuit u2(out_sec,out_min,out_hour,hz64out,out_rstall,toSecond,toMinute,toHour,setZero);
+    ControlUnit u2(out_sec,out_min,out_hour,hz64out,out_rstall,toSecond,toMinute,toHour,setZero);
     
     wire clksec_out;
     wire [3:0]temsecl,temsech;
@@ -77,6 +77,6 @@ module allfile(
     seg s(.hour1(hourH), .hour2(hourL), .min1(minH), .min2(minL), .sec1(secH), .sec2(secL), .clk(hz500out), .rst_n(resetall),  .scan_cnt(scan_cnt), .seg_out(seg_out));
     
     
-    clock u8(secH,secL,minH,minL,hz500out,hz1000out,clk_buzzer);
+    clock u8(.sech(temsech),.secl(temsecl),.minh(temminh),.minl(temminl),.hz500clk(hz500out),.hz1Kclk(hz1000out),.clkout(clk_buzzer));
         
     endmodule
